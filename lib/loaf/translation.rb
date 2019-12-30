15c76befcfab714151fe7a48428877c812d0db70
Ignas Jurjonas <aidzas@gmail.com>
2019-12-30 17:10:38 +0200

    without translation

--- lib/loaf/translation.rb
+++ lib/loaf/translation.rb
@@ -25,12 +25,7 @@ module Loaf
     #
     # @api public
     def find_title(title, options = {})
-      return title if title.nil? || title.empty?
-
-      options[:scope] ||= translation_scope
-      options[:default] = Array(options[:default])
-      options[:default] << title if options[:default].empty?
-      I18n.t(title.to_s, options)
+      return title
     end
     module_function :find_title
   end # Translation
